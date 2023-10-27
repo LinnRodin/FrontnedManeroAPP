ca5b233f1f7690190748dcd3d6dd9ac862336394
Linn Rodin Sjögren <ael.rodin@gmail.com>
2023-10-27 21:16:47 +0200

    Deleted from develop
    Deleted from develop moved to products.

--- maneroapp/src/App.js
+++ maneroapp/src/App.js
@@ -1,14 +1,10 @@
 import './App.min.css';
-import CategoriesView from './View/_CategoriesView';
-import GridCategoriesView from './View/_GridCategoriesView';
-import DownBarCategories from './View/_DownBarCategories';
+
 
 function App() {
   return (   
     <>
-      <CategoriesView/>
-      <GridCategoriesView/>
-      <DownBarCategories/>
+    
     </>
   );
 }
