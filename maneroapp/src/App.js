983b91105cffeb06ec62ef3f3f7f7e357fb8ec35
Linn Rodin Sjögren <ael.rodin@gmail.com>
2023-10-27 21:30:43 +0200

    Product-branch updated
    Product-branch updated

--- maneroapp/src/App.js
+++ maneroapp/src/App.js
@@ -1,10 +1,15 @@
 import './App.min.css';
+import './View/CategoriesView';
+import './View/GridCategoriesView';
+import'./View/DownBarCategories';
 
 
 function App() {
   return (
     <>
-       
+       <CategoriesView/>
+       <GridCategoriesView/>
+       <DownBarCategories/> 
     </>
   );
 }
---
+++ maneroapp/src/View/DownBarCategories.js
@@ -0,0 +1,19 @@
+import React from 'react'
+
+const _DownBarCategories = () => {
+  return (
+    <section className="down-bar">
+        <div className="container">
+            <div class="bar-icon">
+                <a href="home.html"><i className="fa-solid fa-house"></i></a>
+                <a href="search.html"><i className="fa-regular fa-magnifying-glass"></i></a>
+                <a href="shopping.html"><i className="fa-light fa-bag-shopping"></i></a>
+                <a href="wishlist.html"><i className="fa-regular fa-heart"></i></a>
+                <a href="signup.html"><i className="fa-regular fa-user"></i></a>
+            </div>
+        </div>
+    </section>
+  )
+}
+
+export default _DownBarCategories
