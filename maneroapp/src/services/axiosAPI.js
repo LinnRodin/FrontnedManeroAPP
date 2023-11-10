153fef8a504510a1f0bf2a15bc569a342692380c
Linn Rodin Sjögren <ael.rodin@gmail.com>
2023-11-10 21:18:18 +0100

    Update
    Update

--- maneroapp/src/services/axiosAPI.js
+++ maneroapp/src/services/axiosAPI.js
@@ -3,7 +3,7 @@ import axios from 'axios';
 // Skapar en instans av axios med förkonfigurerade inställningar
 const axiosAPI = axios.create({
   // Bas-URL:en för backend manero
-  baseURL: 'https://localhost:7277/',
+  baseURL: 'https://localhost:7277',
   // Definierar timeouts
   timeout: 3000,
   // Definierar headers
